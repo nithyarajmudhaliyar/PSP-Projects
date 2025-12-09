7e6607e0e4dfeba10321a964ac82660e1c0695ab
Nithyaraj <nithyarajmudhaliyar2007@gmail.com>
2025-12-09 22:03:54 +0530

    Implement grading logic for student answers
    Added grading logic to evaluate student answers against a key.

--- AutomaticGrader.py
+++ AutomaticGrader.py
@@ -5,3 +5,13 @@ students = [
     ["Jane", ['A', 'A', 'A', 'A', 'A']],
     ["Bob",  ['B', 'A', 'D', 'D', 'C']]
 ]
+for student in students:
+    name = student[0]
+    answers = student[1]
+    if len(answers) != len(key):
+        print(f'{name} is invalid')
+    score = 0
+    for i in range(len(key)):
+        if answers[i] == key[i]:
+            score += 1
+    print(f'{name}:{score}/{len(key)}')
